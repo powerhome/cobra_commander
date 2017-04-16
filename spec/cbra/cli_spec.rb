68e72839c5e0aac9dd6a879c3c0a698cee9c21d4
Ben Langfeld <blangfeld@powerhrg.com>
2017-04-16 15:32:56 -0300

    Lint

--- spec/cbra/cli_spec.rb
+++ spec/cbra/cli_spec.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe "cli", type: :aruba do
-  let(:file) { "file.txt" }
-  let(:content) { "Hello World" }
-
   describe "checking the version" do
     it "reports the current version" do
       run_simple("cbra version", fail_on_error: true)
