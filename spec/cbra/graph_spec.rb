adbd23d10a3ca694614268eb8f4b286e4505a8c5
Garett Arrowood <garettarrowood@gmail.com>
2017-05-03 08:46:19 -0400

    Generate semi-static dot file for test

--- spec/cbra/graph_spec.rb
+++ spec/cbra/graph_spec.rb
@@ -4,13 +4,6 @@ require "spec_helper"
 
 RSpec.describe Cbra::Graph do
   describe "#generate!" do
-    let(:fixture_root) do
-      File.expand_path(
-        "../fixtures",
-        File.dirname(__FILE__)
-      )
-    end
-
     let(:root) do
       File.expand_path(
         "../..",
@@ -18,14 +11,56 @@ RSpec.describe Cbra::Graph do
       )
     end
 
-    it "correctly generates graph.dot" do
-      Cbra::Graph.new(File.join(fixture_root, "app"), "dot").generate!
+    let!(:native_graph) do
+      graph = GraphViz.new(:G, type: :digraph, concentrate: true)
 
-      fixture_dot = File.join(fixture_root, "graph.dot")
+      # Add 3 nodes
+      a = graph.add_nodes("a")
+      b = graph.add_nodes("b")
+      c = graph.add_nodes("c")
+
+      # Connect b & c to a
+      graph.add_edges(a, b)
+      graph.add_edges(a, c)
+
+      # Create graph
+      graph.output(dot: "native_graph.dot")
+    end
+
+    let(:hash_tree) do
+      # Hash setup identical to native_graph construction
+      {
+        name: "a",
+        dependencies: [
+          {
+            name: "b",
+            dependencies: [],
+          },
+          {
+            name: "c",
+            dependencies: [],
+          },
+        ],
+      }
+    end
+
+    before do
+      allow_any_instance_of(Cbra::ComponentTree).to receive(:to_h).and_return(hash_tree)
+    end
+
+    it "correctly generates graph.dot" do
+      Cbra::Graph.new(".", "dot").generate!
+
+      native_dot = File.join(root, "native_graph.dot")
       generated_dot = File.join(root, "graph.dot")
 
-      expect(IO.readlines(fixture_dot)).to eq(IO.readlines(generated_dot))
+      expect(IO.readlines(native_dot)).to eq(IO.readlines(generated_dot))
+    end
+
+    after do
+      # Cleanup extra files
+      `rm native_graph.dot`
+      `rm graph.dot`
     end
   end
 end
-
