935e18bb5716c730e992ac5056aa89eb91a642f6
Garett Arrowood <garettarrowood@gmail.com>
2017-05-04 13:01:54 -0400

    Add affected unit tests

--- spec/cbra/affected_spec.rb
+++ spec/cbra/affected_spec.rb
@@ -4,24 +4,45 @@ require "spec_helper"
 
 RSpec.describe Cbra::Affected do
   before(:all) do
-    @path = AppHelper.root
+    @root = AppHelper.root
     @tree = AppHelper.tree
   end
 
   describe "::new" do
-    let(:no_changes) do
-      described_class.new(@tree, [], @path)
-    end
-
-    let(:with_changes) do
-      # described_class.new(@tree, [stuff], @path)
-    end
-
     it "successfully instantiates" do
-      expect(described_class.new(@tree, [], @path)).to be_truthy
+      expect(described_class.new(@tree, [], @root)).to be_truthy
     end
 
     context "with no changes" do
+      let(:no_changes) do
+        described_class.new(@tree, [], @root)
+      end
+
+      it "reports no directly affected components" do
+        expect(no_changes.directly).to eq []
+      end
+
+      it "reports no transitiely affected components" do
+        expect(no_changes.transitively).to eq []
+      end
+    end
+
+    context "with change to top level dependency" do
+      let(:with_change_to_a) do
+        described_class.new(@tree, ["#{@root}/components/a/Gemfile"], @root)
+      end
+
+      it "correctly reports directly affected components" do
+        expect(with_change_to_a.directly).to eq [
+          { name: "a",
+            path: "#{@root}/components/a",
+          }
+        ]
+      end
+
+      it "correctly reports directly affected components" do
+        expect(with_change_to_a.transitively).to eq []
+      end
     end
   end
 end
