b671f93c725759a869e9026265ec68bcd01d959d
Garett Arrowood <garettarrowood@gmail.com>
2017-08-31 10:59:44 -0400

    Create Ruby & Js containers for logic

--- lib/cobra_commander/component_tree.rb
+++ lib/cobra_commander/component_tree.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "json"
-require "ostruct"
 
 module CobraCommander
   # Representation of the tree of components and their dependencies
@@ -11,15 +10,17 @@ module CobraCommander
     end
 
     def to_h
-      TreeGenerator.new(UMBRELLA_APP_NAME, @root_path).to_h
+      Tree.new(UMBRELLA_APP_NAME, @root_path).to_h
     end
 
     # Generates component tree
-    class TreeGenerator
+    class Tree
       def initialize(name, path, ancestry = Set.new)
         @name = name
         @root_path = path
         @ancestry = ancestry
+        @ruby = Ruby.new(path)
+        @js = Js.new(path)
         @type = type_of_component
       end
 
@@ -36,79 +37,95 @@ module CobraCommander
     private
 
       def type_of_component
-        return "Ruby & JS" if gem? && node?
-        return "Ruby" if gem?
-        return "JS" if node?
+        return "Ruby & JS" if @ruby.gem? && @js.node?
+        return "Ruby" if @ruby.gem?
+        return "JS" if @js.node?
       end
 
       def dependencies
         @deps ||= begin
-          deps = ruby_dependencies + js_dependencies
-          deps.sort_by(&:name)
+          deps = @ruby.dependencies + @js.dependencies
+          deps.sort_by { |dep| dep[:name] }
         end
-      end
-
-      def ruby_dependencies
-        @ruby_dependencies ||= begin
-          return [] unless gem?
-          bundler_definition.dependencies.select do |dep|
-            dep.source&.is_a_path? && dep.source.path.to_s != "."
-          end
-        end
-      end
-
-      def js_dependencies
-        @nodes ||= begin
-          return [] unless node?
-          json = JSON.parse(File.read(package_json_path))
-          js_format(json["dependencies"])
-        end
-      end
-
-      def js_format(deps)
-        return [] if deps.nil?
-        linked_deps = deps.select { |_, v| v.start_with? "link:" }
-        linked_deps.map do |_, v|
-          relational_path = v.split("link:")[1]
-          dep_name = relational_path.split("/")[-1]
-          OpenStruct.new(name: dep_name, path: relational_path)
-        end
-      end
-
-      def bundler_definition
-        ::Bundler::Definition.build(gemfile_path, gemfile_lock_path, nil)
-      end
-
-      def gem?
-        @gem ||= File.exist?(gemfile_path)
-      end
-
-      def node?
-        @node ||= File.exist?(package_json_path)
-      end
-
-      def package_json_path
-        File.join(@root_path, "package.json")
-      end
-
-      def gemfile_path
-        File.join(@root_path, "Gemfile")
-      end
-
-      def gemfile_lock_path
-        File.join(@root_path, "Gemfile.lock")
       end
 
       def dep_representation(dep)
-        dep_path, dep_name = extract_dep_info(dep)
+        full_path = File.expand_path(File.join(@root_path, dep[:path]))
         ancestry = @ancestry + [{ name: @name, path: @root_path, type: @type }]
-        self.class.new(dep_name, dep_path, ancestry).to_h
+        self.class.new(dep[:name], full_path, ancestry).to_h
       end
 
-      def extract_dep_info(dep)
-        relational_path = dep.is_a?(OpenStruct) ? dep.path : "#{dep.source.path}/#{dep.name}"
-        path = File.expand_path(File.join(@root_path, relational_path))
-        [path, dep.name]
+      # Calculates ruby dependencies
+      class Ruby
+        def initialize(root_path)
+          @root_path = root_path
+        end
+
+        def dependencies
+          @deps ||= begin
+            return [] unless gem?
+            gems = bundler_definition.dependencies.select do |dep|
+              dep.source&.is_a_path? && dep.source.path.to_s != "."
+            end
+            format(gems)
+          end
+        end
+
+        def format(deps)
+          deps.map do |dep|
+            path = File.join(dep.source.path, dep.name)
+            { name: dep.name, path: path }
+          end
+        end
+
+        def gem?
+          @gem ||= File.exist?(gemfile_path)
+        end
+
+        def bundler_definition
+          ::Bundler::Definition.build(gemfile_path, gemfile_lock_path, nil)
+        end
+
+        def gemfile_path
+          File.join(@root_path, "Gemfile")
+        end
+
+        def gemfile_lock_path
+          File.join(@root_path, "Gemfile.lock")
+        end
+      end
+
+      # Calculates js dependencies
+      class Js
+        def initialize(root_path)
+          @root_path = root_path
+        end
+
+        def dependencies
+          @deps ||= begin
+            return [] unless node?
+            json = JSON.parse(File.read(package_json_path))
+            format(json["dependencies"])
+          end
+        end
+
+        def format(deps)
+          return [] if deps.nil?
+          linked_deps = deps.select { |_, v| v.start_with? "link:" }
+          linked_deps.map do |_, v|
+            relational_path = v.split("link:")[1]
+            dep_name = relational_path.split("/")[-1]
+            { name: dep_name, path: relational_path }
+          end
+        end
+
+        def node?
+          @node ||= File.exist?(package_json_path)
+        end
+
+        def package_json_path
+          File.join(@root_path, "package.json")
+        end
       end
     end
   end
