fc9c525efca5b2f3870c1dbc48b4b84ba8eb480d
Carlos Palhares <chjunior@gmail.com>
2020-07-28 10:38:47 -0300

    Fix executor so a nested bundle call can succeed

--- lib/cobra_commander/executor.rb
+++ lib/cobra_commander/executor.rb
@@ -4,11 +4,13 @@ module CobraCommander
   # Execute commands on all components of a ComponentTree
   module Executor
     def self.exec(components, command, printer = $stdout)
-      components.each do |component|
-        component.root_paths.each do |path|
-          printer.puts "===> #{component.name} (#{path})"
-          output, = Open3.capture2e(command, chdir: path, unsetenv_others: true)
-          printer.puts output
+      Bundler.with_original_env do
+        components.each do |component|
+          component.root_paths.each do |path|
+            printer.puts "===> #{component.name} (#{path})"
+            output, = Open3.capture2e(command, chdir: path)
+            printer.puts output
+          end
         end
       end
     end
