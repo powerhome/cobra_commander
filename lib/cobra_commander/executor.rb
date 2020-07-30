13b397b62e555ae6f69f5a7b7b216a45aa5d6e68
Carlos Palhares <chjunior@gmail.com>
2020-07-30 18:46:51 -0300

    Replace old executor by tty based

--- lib/cobra_commander/executor.rb
+++ lib/cobra_commander/executor.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
+require_relative "executor/component_exec"
+require_relative "executor/multi_exec"
+
 module CobraCommander
   # Execute commands on all components of a ComponentTree
   module Executor
-    def self.exec(components, command, printer = $stdout)
-      Bundler.with_original_env do
-        components.each do |component|
-          component.root_paths.each do |path|
-            printer.puts "===> #{component.name} (#{path})"
-            output, = Open3.capture2e(command, chdir: path)
-            printer.puts output
-          end
-        end
-      end
+    def self.exec(components, command, output = $stdout, status_output = $stderr)
+      components = Array(components)
+      exec = components.size == 1 ? ComponentExec.new(components.first)
+                                  : MultiExec.new(components)
+      exec.run(command, output: output, spin_output: status_output)
     end
   end
 end
