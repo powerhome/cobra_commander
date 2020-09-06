86d1ac67c96463607023f6de7b7837939a6cdc13
Carlos Palhares <chjunior@gmail.com>
2020-09-06 13:42:32 -0300

    Fix bundle binstubs setting BUNDLE_GEMFILE on original env

--- spec/cobra_commander/executor/component_exec_spec.rb
+++ spec/cobra_commander/executor/component_exec_spec.rb
@@ -5,19 +5,37 @@ require "cobra_commander/executor/component_exec"
 
 RSpec.describe CobraCommander::Executor::ComponentExec do
   let(:output) { StringIO.new }
-  let(:component_e) { fixture_umbrella.find("e") }
-  subject { CobraCommander::Executor::ComponentExec.new(component_e) }
+  let(:component_d) { fixture_umbrella.find("d") }
+  subject { CobraCommander::Executor::ComponentExec.new(component_d) }
 
   it "executes in the context of the given component" do
     subject.run("echo 'I am at' $PWD", output: output)
 
-    expect(output.string).to include("I am at #{component_e.root_paths.first}")
+    expect(output.string).to include("I am at #{component_d.root_paths.first}")
   end
 
   it "cleans BUNDLE environment so a nested bundle call can succeed" do
     subject.run("bundle exec env", output: output)
 
     expect(output.string).to_not include("Install missing gem executables with")
+  end
+
+  it "executes the command in the given bundle context" do
+    Bundler::ORIGINAL_ENV["BUNDLE_GEMFILE"] = File.expand_path(
+      "../../../../Gemfile",
+      Pathname.new(__FILE__).realpath
+    )
+
+    subject.run("bundle list", output: output)
+
+    expect(output.string).to eql <<~OUT
+      Gems included by the bundle:
+        * b (0.0.1)
+        * bundler (1.17.3)
+        * c (0.0.1)
+        * d (0.0.1)
+        * rake (12.0.0)
+    OUT
   end
 
   it "allows to override cmd options" do
