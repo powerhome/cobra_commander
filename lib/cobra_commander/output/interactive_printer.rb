df8aea362b07ef6a8d0e0d9d0def9893e196a2ea
Carlos Palhares <chjunior@gmail.com>
2021-11-24 16:41:52 -0300

    Retain selection on interactive printer

--- lib/cobra_commander/output/interactive_printer.rb
+++ lib/cobra_commander/output/interactive_printer.rb
@@ -18,23 +18,28 @@ module CobraCommander
 
       def initialize(contexts)
         @prompt = TTY::Prompt.new
-        @options = contexts.sort(&method(:sort_contexts))
-          .reduce({}) do |options, context|
-          template = context.success? ? SUCCESS : ERROR
-          options.merge format(template, context.component_name) => context
-        end
+        @options = map_options(contexts)
       end
 
       def run(output)
+        selected_context = nil
         loop do
-          context = @prompt.select("Print output?", @options)
-          output.puts context.output
+          selected_context = @prompt.select("Print output?", @options, default: @options.key(selected_context))
+          output.puts selected_context.output
         end
       rescue TTY::Reader::InputInterrupt
         output.puts BYE
       end
 
       private
+
+      def map_options(contexts)
+        contexts.sort(&method(:sort_contexts))
+                .reduce({}) do |options, context|
+                  template = context.success? ? SUCCESS : ERROR
+                  options.merge format(template, context.component_name) => context
+                end
+      end
 
       def sort_contexts(context_a, context_b)
         if context_a.success? == context_b.success?
