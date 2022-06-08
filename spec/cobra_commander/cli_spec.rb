a494bad34b47a9361bc4c4f4d42db2a36a03146e
Carlos Palhares <chjunior@gmail.com>
2022-06-08 14:55:22 -0300

    Clear rubocop offenses

--- spec/cobra_commander/cli_spec.rb
+++ spec/cobra_commander/cli_spec.rb
@@ -73,22 +73,28 @@ RSpec.describe "cobra cli", type: :aruba do
     end
 
     it "executes the given command a given component's js dependencies without self optionally" do
-      run_command_and_stop("cobra exec --no-interactive -a #{fixture_app} --js --dependencies --no-self h 'touch cobra-rocks'",
-                           fail_on_error: true)
+      run_command_and_stop(
+        "cobra exec --no-interactive -a #{fixture_app} --js --dependencies --no-self h 'touch cobra-rocks'",
+        fail_on_error: true
+      )
 
       expect(components_affected).to match_array %w[b e f]
     end
 
     it "executes the given command a given component's js dependencies including own component by default" do
-      run_command_and_stop("cobra exec --no-interactive -a #{fixture_app} --js --dependencies h 'touch cobra-rocks'",
-                           fail_on_error: true)
+      run_command_and_stop(
+        "cobra exec --no-interactive -a #{fixture_app} --js --dependencies h 'touch cobra-rocks'",
+        fail_on_error: true
+      )
 
       expect(components_affected).to match_array %w[b e f h]
     end
 
     it "executes the given command a given component's ruby dependencies" do
-      run_command_and_stop("cobra exec --no-interactive -a #{fixture_app} --ruby --dependencies --no-self h 'echo \"hello from\" `pwd`'",
-                           fail_on_error: true)
+      run_command_and_stop(
+        "cobra exec --no-interactive -a #{fixture_app} --ruby --dependencies --no-self h 'echo \"hello from\" `pwd`'",
+        fail_on_error: true
+      )
 
       expect(last_command_output).to include("hello from #{fixture_app}/components/b")
     end
