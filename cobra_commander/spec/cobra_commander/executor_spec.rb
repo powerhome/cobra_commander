9162ab0144766854df383bdf04c4f2b8083461a3
Carlos Palhares <chjunior@gmail.com>
2022-11-09 21:25:14 -0300

    Remove fixture_umbrella dependency from Executor spec
    executor spec

--- cobra_commander/spec/cobra_commander/executor_spec.rb
+++ cobra_commander/spec/cobra_commander/executor_spec.rb
@@ -5,27 +5,37 @@ require "cobra_commander/executor"
 
 RSpec.describe CobraCommander::Executor do
   let(:spin_output) { StringIO.new }
-  let(:component_e) { fixture_umbrella("app").find("e") }
-  subject { CobraCommander::Executor.new(component_e.dependents, concurrency: 1, spin_output: spin_output) }
+  let(:component_path) { Pathname.new(__dir__) }
+  let(:package) { CobraCommander::Package.new(nil, path: component_path, dependencies: [], name: nil) }
+  let(:component) { CobraCommander::Component.new(nil, "component_a") }
+  let(:another_component_path) { fixture_path }
+  let(:another_package) { CobraCommander::Package.new(nil, path: another_component_path, dependencies: [], name: nil) }
+  let(:another_component) { CobraCommander::Component.new(nil, "component_b") }
   before do
+    component.add_package(package)
+    another_component.add_package(another_package)
+
+    # TTY::Spinner::Multi will not print if it's not a TTY IO
     allow(spin_output).to receive(:tty?) { true }
+  end
+  subject do
+    ::CobraCommander::Executor.new([component, another_component], concurrency: 1, spin_output: spin_output)
   end
 
   it "executes in the context of each given component" do
     contexts = subject.exec("echo 'I am at' $PWD")
-    outputs = contexts.map(&:output).join
+    outputs = contexts.map(&:output)
 
-    expect(outputs).to match(%r{I am at .*components/b$})
-    expect(outputs).to match(/I am at .*node_manifest$/)
-    expect(outputs).to match(%r{I am at .*components/g$})
+    expect(contexts.size).to eql 2
+    expect(outputs.first).to match(/I am at #{component_path}$/)
+    expect(outputs.last).to match(/I am at #{another_component_path}$/)
   end
 
   it "prints the status of each component" do
     subject.exec("echo 'I am at' $PWD")
 
-    expect(spin_output.string).to match(/\[DONE\](\e\[0m)? b/)
-    expect(spin_output.string).to match(/\[DONE\](\e\[0m)? node_manifest/)
-    expect(spin_output.string).to match(/\[DONE\](\e\[0m)? g/)
+    expect(spin_output.string).to match(/\[DONE\](\e\[0m)? component_a/)
+    expect(spin_output.string).to match(/\[DONE\](\e\[0m)? component_b/)
   end
 
   it "fails when the command fail" do
