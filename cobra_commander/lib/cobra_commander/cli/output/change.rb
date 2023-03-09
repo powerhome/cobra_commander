d46d8896b69434ac37df1a6613316419e39474b5
Carlos Palhares <chjunior@gmail.com>
2023-03-09 14:13:18 -0300

    Remove unused output and formats from cobra change

--- cobra_commander/lib/cobra_commander/cli/output/change.rb
+++ cobra_commander/lib/cobra_commander/cli/output/change.rb
@@ -6,27 +6,19 @@ require "cobra_commander/affected"
 module CobraCommander
   class CLI
     module Output
-      # Calculates and prints affected components & files
       class Change
-        InvalidSelectionError = Class.new(StandardError)
-
-        def initialize(umbrella, oformat, branch, changes: nil)
-          @format = oformat
+        def initialize(umbrella, branch, changes: nil)
           @branch = branch
           @umbrella = umbrella
           @changes = changes || GitChanged.new(umbrella.path, branch)
         end
 
         def run!
-          assert_valid_result_choice
-          if selected_format?("json")
-            puts affected.to_json
-          else
-            show_full if selected_format?("full")
-            tests_to_run
-          end
-        rescue GitChanged::InvalidSelectionError => e
-          puts e.message
+          print_changes_since_last_commit
+          puts
+          print_directly_affected_components
+          puts
+          print_transitively_affected_components
         end
 
       private
@@ -35,55 +27,23 @@ module CobraCommander
           @affected ||= Affected.new(@umbrella, @changes)
         end
 
-        def show_full
-          changes_since_last_commit
-          directly_affected_components
-          transitively_affected_components
-        end
-
-        def assert_valid_result_choice
-          return if %w[test full name json].include?(@format)
-
-          raise InvalidSelectionError, "--results must be 'test', 'full', 'name' or 'json'"
-        end
-
-        def selected_format?(result)
-          @format == result
-        end
-
-        def changes_since_last_commit
+        def print_changes_since_last_commit
           puts "<<< Changes since last commit on #{@branch} >>>"
           puts(*@changes) if @changes.any?
-          puts blank_line
         end
 
-        def directly_affected_components
+        def print_directly_affected_components
           puts "<<< Directly affected components >>>"
-          affected.directly.each { |component| puts display(component) }
-          puts blank_line
+          affected.directly.each { |component| display(component) }
         end
 
-        def transitively_affected_components
+        def print_transitively_affected_components
           puts "<<< Transitively affected components >>>"
-          affected.transitively.each { |component| puts display(component) }
-          puts blank_line
-        end
-
-        def tests_to_run
-          puts "<<< Test scripts to run >>>" if selected_format?("full")
-          if selected_format?("name")
-            affected.names.each { |component_name| puts component_name }
-          else
-            affected.scripts.each { |component_script| puts component_script }
-          end
+          affected.transitively.each { |component| display(component) }
         end
 
         def display(component)
-          "#{component.name} - #{component.packages.map(&:key).map(&:to_s).map(&:capitalize).join(' & ')}"
-        end
-
-        def blank_line
-          ""
+          puts "#{component.name} - #{component.packages.map(&:key).map(&:to_s).map(&:capitalize).join(' & ')}"
         end
       end
     end
