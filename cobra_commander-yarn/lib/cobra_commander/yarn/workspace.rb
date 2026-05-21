7dcf888a996f0b0b952e925237993ee5b6aa9403
Carlos Palhares <carlos.palhares@powerhrg.com>
2026-05-21 10:44:28 -0300

    Support newer versions of yarn

--- cobra_commander-yarn/lib/cobra_commander/yarn/workspace.rb
+++ cobra_commander-yarn/lib/cobra_commander/yarn/workspace.rb
@@ -22,10 +22,25 @@ module CobraCommander
     private
 
       def workspace_data
+        output, _error, status = Open3.capture3("yarn workspaces list --verbose --json", chdir: path.to_s)
+        return parse_modern_output(output) if status.success?
+
         output, error, status = Open3.capture3("yarn workspaces --json info", chdir: path.to_s)
-        raise ::CobraCommander::Source::Error, json_data(error) unless status.success?
+        raise ::CobraCommander::Source::Error, "Unrecognized yarn workspaces output format" unless status.success?
 
         JSON.parse(json_data(output))
+      end
+
+      def parse_modern_output(output)
+        entries = output.lines.map(&:strip).reject(&:empty?).map { |line| JSON.parse(line) }
+                        .reject { |e| e["location"] == "." }
+        location_to_name = entries.each_with_object({}) { |e, h| h[e["location"]] = e["name"] }
+        entries.each_with_object({}) do |entry, hash|
+          hash[entry["name"]] = {
+            "location" => entry["location"],
+            "workspaceDependencies" => entry["workspaceDependencies"].filter_map { |loc| location_to_name[loc] }
+          }
+        end
       end
 
       def json_data(json)
